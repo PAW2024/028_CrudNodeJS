9e577afacec0636b6453c311447bf8a0af199bbe
Lintangms14 <merayusukmalintang@gmail.com>
2024-10-31 23:20:29 +0700

    DBCRUD

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const todoroutes = require('./routes/todo.js');
-const port = 3000;
-
+const tododbroutes = require('./routes/tododb.js');
+const port = process.env.PORT || 6000;
+ 
 app.use(express.json());
 
-app.use('/todos',todoroutes);
+app.use('/todos',tododbroutes);
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index");
